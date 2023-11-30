75de527e2f7c2ed4e4542cedce4191ee1cae9145
Pedro Seoane <info@pedroseoaneprado.es>
2023-11-30 19:51:45 +0100

    Mejorado el menú de navegación.

--- js/navbar.js
+++ js/navbar.js
@@ -3,6 +3,32 @@ fetch('../templates/navbar.html')
     .then(content => {
         const navegacion = document.getElementById('navegacion');
         navegacion.innerHTML = content;
+        const pageId = document.querySelector('meta[name="page-id"]').getAttribute('content');
+        const navLinks = document.querySelectorAll('.navegacion a');
+        for (let i = 0; i < navLinks.length; i++) {
+            navLinks[i].classList.remove('active');
+            if (navLinks[i].getAttribute('href').endsWith(`${pageId}.html`)) {
+                navLinks[i].classList.add('active');
+            }
+        }
+        document.getElementById('menuSiPequeño').addEventListener('click', function (event) {
+            event.preventDefault();
+            if (window.innerWidth < 800) {
+                const dropdownMenu = document.querySelector('.dropdown');
+                if (dropdownMenu.style.display === 'block') {
+                    dropdownMenu.style.display = 'none';
+                } else {
+                    dropdownMenu.style.display = 'block';
+                }
+            }
+        });
+
+        window.addEventListener('resize', function () {
+            if (window.innerWidth > 800) {
+                const dropdownMenu = document.querySelector('.dropdown');
+                dropdownMenu.style.display = 'block';
+            }
+        });
     });
 
 fetch('../templates/footer.html')
