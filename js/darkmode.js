0dd7b7915f5c363cb9620c5ea7aba645b4609f0f
Pedro Seoane <info@pedroseoaneprado.es>
2023-12-01 21:06:45 +0100

    Problema de sincronía con Js

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('load', function() {
     const storedTheme = localStorage.getItem('theme') || 'light';
     document.documentElement.setAttribute('data-theme', storedTheme);
 
