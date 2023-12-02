52a9a7dfa014b85ce02061b47a14de0fcca32204
Pedro Seoane <info@pedroseoaneprado.es>
2023-12-02 11:29:00 +0100

    JS dinámico para carga de juegos.js

---
+++ js/juegos.js
@@ -0,0 +1,44 @@
+document.querySelectorAll(".miniaturaJuego").forEach(function (gameElement) {
+  gameElement.addEventListener("click", function () {
+    const existingGame = document.querySelector(".juegoActivo");
+    if (existingGame) {
+      document.body.removeChild(existingGame);
+    }
+
+    const gameName = this.id.replace("miniatura", "").toLowerCase();
+
+    const gameContainer = document.createElement("div");
+    gameContainer.className = "juegoActivo";
+
+    const closeButton = document.createElement("button");
+    closeButton.textContent = "";
+    closeButton.addEventListener("click", function () {
+      document.body.removeChild(gameContainer);
+      location.reload();
+    });
+
+    gameContainer.appendChild(closeButton);
+
+    const canvas = document.createElement("canvas");
+    canvas.id = `canvas${gameName.charAt(0).toUpperCase() + gameName.slice(1)}`;
+    canvas.width = 800; // Set the width of the canvas
+    canvas.height = 600; // Set the height of the canvas
+    gameContainer.appendChild(canvas);
+
+    document.body.appendChild(gameContainer);
+
+    const script = document.createElement("script");
+    script.id = `${gameName}Script`;
+    script.src = `../js/${gameName}.js`;
+    script.defer = true;
+
+    document.body.appendChild(script);
+
+    const cssLink = document.createElement("link");
+    cssLink.id = `${gameName}Css`;
+    cssLink.rel = "stylesheet";
+    cssLink.href = "../css/canvas.css";
+
+    document.head.appendChild(cssLink);
+  });
+});
